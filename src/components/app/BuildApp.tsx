81ee7ef75ee1b35333f65855212bb518ef5309eb
carlos-hereee <97hernandez.c@gmail.com>
2023-11-03 20:52:33 -0500

    use Form for app initialization

--- src/components/app/BuildApp.tsx
+++ src/components/app/BuildApp.tsx
@@ -1,54 +1,31 @@
 import { useContext } from "react";
-import { PaginateForm } from "nexious-library";
+import { Form } from "nexious-library";
 import { useNavigate } from "react-router-dom";
 import { AdminContext } from "@app/utils/context/admin/AdminContext";
-import { FormValueProps } from "app-forms";
 import { AppContext } from "@app/utils/context/app/AppContext";
-import { AuthContext } from "@app/utils/context/auth/AuthContext";
 
 const BuildApp = () => {
-  const { landingPageForm, appNameForm, initApp, formErrors } = useContext(AdminContext);
+  const { appNameForm, initApp } = useContext(AdminContext);
   const { appList } = useContext(AppContext);
   // const { ownedApps } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  const paginate: FormValueProps[] = [
-    {
-      formName: "appName",
-      heading: "Initialize your app",
-      initialValues: appNameForm.initialValues,
-      types: appNameForm.types,
-      labels: appNameForm.labels,
-      placeholders: appNameForm.placeholders,
-      submitLabel: "Save and continue",
-      schema: {
-        required: ["appName", "logo"],
-        unique: [{ name: "appName", list: appList?.map((app) => app.appName) || [] }],
-      },
-      onSubmit: initApp,
-    },
-    {
-      formName: "landing",
-      heading: "Build landing page",
-      initialValues: landingPageForm.initialValues,
-      submitLabel: "Save and continue",
-      labels: landingPageForm.labels,
-      types: landingPageForm.types,
-      placeholders: landingPageForm.placeholders,
-    },
-  ];
-  // console.log("appList :>> ", appList);
-  // console.log("formErrors", formErrors);
-  // console.log("apps", apps);
   return (
     <div className="container">
-      {paginate && paginate.length > 0 && (
-        <PaginateForm
-          paginate={paginate}
-          onCancel={() => navigate("/")}
-          responseError={formErrors.initAppFormError}
-        />
-      )}
+      <Form
+        initialValues={{ appName: "", logo: "" }}
+        onSubmit={initApp}
+        heading="Initialize your app!"
+        submitLabel="Save and continue"
+        types={appNameForm.types}
+        labels={appNameForm.labels}
+        placeholders={appNameForm.placeholders}
+        onCancel={() => navigate("/")}
+        schema={{
+          required: ["appName", "logo"],
+          unique: [{ name: "appName", list: appList?.map((app) => app.appName) || [] }],
+        }}
+      />
     </div>
   );
 };
