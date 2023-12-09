997f1a5fdf05749f90a456809764830a13e65989
carlos-hereee <97hernandez.c@gmail.com>
2023-12-08 19:12:35 -0600

    remove redirect if login

--- src/utils/router/PublicRoute.tsx
+++ src/utils/router/PublicRoute.tsx
@@ -1,11 +1,12 @@
-import { useContext } from "react";
-import { Navigate, Outlet } from "react-router-dom";
-import { AuthContext } from "@context/auth/AuthContext";
+// import { useContext } from "react";
+// import { Navigate, Outlet } from "react-router-dom";
+// import { AuthContext } from "@context/auth/AuthContext";
+import { Outlet } from "react-router-dom";
 
 const PublicRoute = () => {
-  const { accessToken } = useContext(AuthContext);
+  // const { accessToken } = useContext(AuthContext);
 
-  if (accessToken) return <Navigate to="/dashboard" />;
+  // if (accessToken) return <Navigate to="/dashboard" />;
   return <Outlet />;
 };
 export default PublicRoute;
