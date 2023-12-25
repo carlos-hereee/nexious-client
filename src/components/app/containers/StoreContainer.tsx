ceb386f3397f20b270e54b9dd646ef836f93a9ad
carlos-hereee <97hernandez.c@gmail.com>
2023-12-25 10:54:19 -0600

    store comming soon

--- src/components/app/containers/StoreContainer.tsx
+++ src/components/app/containers/StoreContainer.tsx
@@ -1,36 +1,38 @@
-import { AppContext } from "@context/app/AppContext";
-import { PageContainerProps } from "app-types";
-import { Button } from "nexious-library";
-import { useContext } from "react";
-import { formatStoreUrl } from "@app/formatStringUrl";
-import MerchList from "@components/list/MerchList";
-import CopyToClipboard from "../sections/CopyToClipboard";
-import KeyWithDefinition from "../sections/KeyWithDefinition";
+// import { AppContext } from "@context/app/AppContext";
+// import { PageContainerProps } from "app-types";
+// import { Button } from "nexious-library";
+// import { useContext } from "react";
+// import { formatStoreUrl } from "@app/formatStringUrl";
+// import MerchList from "@components/list/MerchList";
+// import CopyToClipboard from "../sections/CopyToClipboard";
+// import KeyWithDefinition from "../sections/KeyWithDefinition";
 
-const StoreContainer = (props: PageContainerProps) => {
-  const { onPhaseClick } = props;
-  const { store, appLink } = useContext(AppContext);
+// const StoreContainer = (props: PageContainerProps) => {
+const StoreContainer = () => {
+  // const { onPhaseClick } = props;
+  // const { store, appLink } = useContext(AppContext);
 
-  if (!store || !store.storeId) {
-    return (
-      <div className="container">
-        <h2 className="heading">Store:</h2>
-        <KeyWithDefinition label="Store details:" labelLayout="bolden">
-          <Button label="+ Create store" onClick={() => onPhaseClick("phase-one")} />
-        </KeyWithDefinition>
-      </div>
-    );
-  }
-  const noInventoryHint = {
-    title: "Hint!",
-    body: "Your inventory is empty click on + add merch to add items to your inventory",
-  };
+  // if (!store || !store.storeId) {
+  //   return (
+  //     <div className="container">
+  //       <h2 className="heading">Store:</h2>
+  //       <KeyWithDefinition label="Store details:" labelLayout="bolden">
+  //         <Button label="+ Create store" onClick={() => onPhaseClick("phase-one")} />
+  //       </KeyWithDefinition>
+  //     </div>
+  //   );
+  // }
+  // const noInventoryHint = {
+  //   title: "Hint!",
+  //   body: "Your inventory is empty click on + add merch to add items to your inventory",
+  // };
 
   return (
     <div className="container">
       <h2 className="heading">Store:</h2>
+      <p>Comming soon</p>
       {/* <KeyWithDefinition label="Store name:" value={store.name || "No name"} labelLayout="bolden" /> */}
-      <KeyWithDefinition label="Store url:" labelLayout="bolden">
+      {/* <KeyWithDefinition label="Store url:" labelLayout="bolden">
         <CopyToClipboard data={formatStoreUrl(appLink, store.name)} />
       </KeyWithDefinition>
       <KeyWithDefinition label="Stripe Settings:" labelLayout="bolden">
@@ -48,7 +50,7 @@ const StoreContainer = (props: PageContainerProps) => {
           <Button label="Edit store details" onClick={() => onPhaseClick("phase-one")} />
           <Button label="+ Add merch" onClick={() => onPhaseClick("phase-two")} />
         </div>
-      </KeyWithDefinition>
+      </KeyWithDefinition> */}
     </div>
   );
 };
