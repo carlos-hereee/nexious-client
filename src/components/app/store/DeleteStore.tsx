18000cd48a9fa7dd28cace9dc4dc6680471a8829
carlos-hereee <97hernandez.c@gmail.com>
2024-05-04 20:06:53 -0500

    add 1 layer of security

--- src/components/app/store/DeleteStore.tsx
+++ src/components/app/store/DeleteStore.tsx
@@ -2,18 +2,39 @@ import { AdminContext } from "@context/admin/AdminContext";
 import { AppContext } from "@context/app/AppContext";
 import { DialogProps } from "app-types";
 import { ButtonCancel, Button } from "nexious-library/@nxs-atoms";
-import { useContext } from "react";
+import { Form } from "nexious-library";
+import { useContext, useState } from "react";
 
 const DeleteStore = ({ onClose }: DialogProps) => {
   const { store, appId } = useContext(AppContext);
   const { deleteStore } = useContext(AdminContext);
+  const [typeConfirm, setConfirm] = useState("");
+  const [error, setError] = useState("");
+
+  const handleConfirm = () => {
+    if (typeConfirm === "CONFIRM") {
+      setError("");
+      deleteStore(appId);
+    } else setError("Must enter 'CONFIRM' to delete");
+  };
+  const handleChange = (e: { value: string }[]) => {
+    const val = e[0].value;
+    setConfirm(val);
+  };
   return (
-    <div className="container">
-      <h2 className="heading">Are you sure you want to delete your store {store?.storeName}</h2>
-      <p>This will delete all progress</p>
+    <div className="container text-center">
+      <h2 className="heading w-max text-center">Are you sure you want to delete your store {store?.storeName}</h2>
+      <p className="w-max">This will delete all progress</p>
+      {error && <p className="required w-max">{error}</p>}
+      <Form
+        theme="form-center"
+        initialValues={{ typeConfirm }}
+        onChange={handleChange}
+        labels={{ typeConfirm: "Type CONFIRM to delete" }}
+      />
       <div className="flex-center">
         <Button label="Go back" onClick={onClose} />
-        <ButtonCancel onClick={() => deleteStore(appId)} theme="btn-main" label="Confirm" />
+        <ButtonCancel onClick={handleConfirm} theme="btn-main" label="Confirm" />
       </div>
     </div>
   );
