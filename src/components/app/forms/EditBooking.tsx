5fa7ed16f17d4666be07918a485b3b0043703501
carlos-hereee <97hernandez.c@gmail.com>
2024-06-22 12:46:59 -0500

    satisfy build

--- src/components/app/forms/EditBooking.tsx
+++ src/components/app/forms/EditBooking.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { AppContext } from "@context/app/AppContext";
 import { Form, Loading } from "nexious-library";
 import { AdminContext } from "@context/admin/AdminContext";
@@ -11,35 +11,35 @@ const EditBooking = () => {
   const { isLoading } = useContext(AppContext);
 
   // test data
-  const [events, setEvents] = useState<AppValues[]>([]);
+  // const [events, setEvents] = useState<AppValues[]>([]);
 
-  const initialValues = { ...calendarBookingForm.initialValues };
+  // const initialValues = { ...calendarBookingForm.initialValues };
   // console.log("events :>> ", events);
   if (isLoading) return <Loading message="Loading app data" />;
-  if (events.length <= 0) {
-    return (
-      <div className="primary-container">
-        <Form
-          initialValues={initialValues}
-          labels={calendarBookingForm.labels}
-          placeholders={calendarBookingForm.placeholders}
-          types={calendarBookingForm.types}
-          fieldHeading={calendarBookingForm.fieldHeading}
-          heading="Create a new booking rule"
-          dataList={{ frequency: cal.calendarData.frequency }}
-          // onSubmit={(values: AppValues) => editCalendar({ values, appId })}
-          onSubmit={(values: AppValues) => setEvents((e: AppValues) => [...e, values])}
-          submitLabel="Save and continue"
-          schema={{ required: ["name", "startTime", "closeTime"] }}
-          noScroll
-        />
-      </div>
-    );
-  }
+  // if (events.length <= 0) {
+  //   return (
+  //     <div className="primary-container">
+  //       <Form
+  //         initialValues={initialValues}
+  //         labels={calendarBookingForm.labels}
+  //         placeholders={calendarBookingForm.placeholders}
+  //         types={calendarBookingForm.types}
+  //         fieldHeading={calendarBookingForm.fieldHeading}
+  //         heading="Create a new booking rule"
+  //         dataList={{ frequency: cal.calendarData.frequency }}
+  //         // onSubmit={(values: AppValues) => editCalendar({ values, appId })}
+  //         // onSubmit={(values: AppValues) => setEvents((e: AppValues) => [...e, values])}
+  //         submitLabel="Save and continue"
+  //         schema={{ required: ["name", "startTime", "closeTime"] }}
+  //         noScroll
+  //       />
+  //     </div>
+  //   );
+  // }
   return (
     <div className="container">
       <Form
-        initialValues={events[0]}
+        initialValues={{}}
         labels={calendarBookingForm.labels}
         placeholders={calendarBookingForm.placeholders}
         types={calendarBookingForm.types}
--- src/utils/context/app/request/stripeAccountLink.tsx
+++ src/utils/context/app/request/stripeAccountLink.tsx
@@ -2,12 +2,11 @@ import { axiosAuth } from "@axios/axiosAuth";
 import { APP_ACTIONS } from "@actions/AppActions";
 import { AppDispatchProps } from "app-context";
 import { AxiosError } from "axios";
-import { DataResponse } from "utils/@types/response";
 
 export const stripeAccountLink = async ({ dispatch, appId }: AppDispatchProps) => {
   try {
     dispatch({ type: APP_ACTIONS.IS_LOADING, payload: true });
-    const { data }: DataResponse = await axiosAuth.post(`/store/stripe-account-link/${appId}`);
+    const { data } = await axiosAuth.post(`/store/stripe-account-link/${appId}`);
     dispatch({ type: APP_ACTIONS.SET_REDIRECT_URL, payload: data });
     dispatch({ type: APP_ACTIONS.IS_LOADING, payload: false });
   } catch (error) {
