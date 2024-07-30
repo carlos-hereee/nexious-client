623ada25281ffe8dd09bb1e62ec3fba5fa9739a7
carlos-hereee <97hernandez.c@gmail.com>
2024-07-30 05:13:05 -0500

    navigate to explore page if no orders have been created

--- src/components/app/containers/TrackOrders.tsx
+++ src/components/app/containers/TrackOrders.tsx
@@ -1,8 +1,9 @@
 import { AuthContext } from "@context/auth/AuthContext";
 import { useContext, useEffect, useState } from "react";
 import { OrderSchema } from "store-context";
-import { Dialog } from "nexious-library";
+import { Button, Dialog } from "nexious-library";
 import { StoreContext } from "@context/store/StoreContext";
+import { useNavigate } from "react-router-dom";
 import ViewOrdersContainer from "./ViewOrdersContainer";
 import OrderClientDetails from "../OrderClientDetails";
 
@@ -10,6 +11,8 @@ const TrackOrder = () => {
   const { orders } = useContext(AuthContext);
   const { trackOrder, setTrackOrder } = useContext(StoreContext);
   const [order, setActiveOrder] = useState<OrderSchema | undefined>();
+  const navigate = useNavigate();
+
   useEffect(() => {
     if (trackOrder) {
       setActiveOrder(trackOrder);
@@ -17,7 +20,13 @@ const TrackOrder = () => {
     }
   }, [trackOrder]);
 
-  if (!orders) return <h2 className="heading">No orders yet</h2>;
+  if (!orders || orders.length === 0)
+    return (
+      <div className="primary-container">
+        <h2 className="heading text-center">No orders yet</h2>
+        <Button label="Explore apps" onClick={() => navigate("/explore")} />
+      </div>
+    );
 
   return (
     <div className="container">
