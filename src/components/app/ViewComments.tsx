642a411ad303b887e99e827245e421061b4bc11e
carlos-hereee <97hernandez.c@gmail.com>
2024-08-08 22:43:40 -0500

    min post width

--- src/components/app/ViewComments.tsx
+++ src/components/app/ViewComments.tsx
@@ -1,17 +1,17 @@
-import { AuthContext } from "@context/auth/AuthContext";
-import { Button, IconButton } from "nexious-library";
-import { useContext } from "react";
+// import { AuthContext } from "@context/auth/AuthContext";
+// import { Button, IconButton } from "nexious-library";
+// import { useContext } from "react";
 
 interface Comments {
   comments: string[];
 }
 const ViewComments = ({ comments }: Comments) => {
-  const { accessToken } = useContext(AuthContext);
+  // const { accessToken } = useContext(AuthContext);
   if (!comments || comments.length === 0)
     return (
       <>
         <p>Be the first to leave a comment </p>
-        {accessToken ? <IconButton icon={{ icon: "reply" }} theme="highlight" /> : <Button label="Create an account" />}
+        {/* {accessToken ? <IconButton icon={{ icon: "reply" }} theme="highlight" /> : <Button label="Create an account" />} */}
       </>
     );
   return <div>VIEW COMMENTS</div>;
--- src/components/app/ViewPosts.tsx
+++ src/components/app/ViewPosts.tsx
@@ -23,28 +23,31 @@ const ViewPosts = ({ posts }: Props) => {
     setActivePost(p);
   };
   return (
-    <div className="primary-container overflow-y">
-      {sortedPosts.length > 0 ? (
-        sortedPosts.map((post) => (
-          <div key={post.uid} className="post">
-            {post.thumbnail && <Hero hero={{ url: post.thumbnail, alt: post.name }} theme="post-thumbnail m-auto p-1" />}
-            <CardTextBubble data={post} />
-            <div className="container p-1">
-              <div className="flex-g">
-                <IconButton icon={{ icon: "heart" }} theme="highlight" />
-                <IconButton
-                  icon={{ icon: "comment" }}
-                  theme={`highlight${post.postId === activePost?.postId ? " btn-active" : ""}`}
-                  onClick={() => handlePostCommentClick(post)}
-                />
+    <div className="split-container">
+      <div className="primary-container overflow-y">
+        {sortedPosts.length > 0 ? (
+          sortedPosts.map((post) => (
+            <div key={post.uid} className="post">
+              {post.thumbnail && <Hero hero={{ url: post.thumbnail, alt: post.name }} theme="post-thumbnail m-auto p-1" />}
+              <CardTextBubble data={post} />
+              <div className="container p-1">
+                <div className="flex-g">
+                  <IconButton icon={{ icon: "heart" }} theme="highlight" />
+                  <IconButton
+                    icon={{ icon: "comment" }}
+                    theme={`highlight${post.postId === activePost?.postId ? " btn-active" : ""}`}
+                    onClick={() => handlePostCommentClick(post)}
+                  />
+                </div>
+                {post.postId === activePost?.postId && <ViewComments comments={post.comments} />}
               </div>
-              {post.postId === activePost?.postId && <ViewComments comments={post.comments} />}
             </div>
-          </div>
-        ))
-      ) : (
-        <p>No posts</p>
-      )}
+          ))
+        ) : (
+          <p>No posts</p>
+        )}
+      </div>
+      <div className="container" />
     </div>
   );
 };
