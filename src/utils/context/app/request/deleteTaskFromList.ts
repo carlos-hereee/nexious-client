5e648976cd1642409c22ca50dcacd402d9891658
carlos-hereee <97hernandez.c@gmail.com>
2024-09-09 06:44:34 -0500

    remove tasks

--- src/utils/context/app/request/deleteTaskFromList.ts
+++ src/utils/context/app/request/deleteTaskFromList.ts
@@ -1,11 +1,13 @@
+import { APP_ACTIONS } from "@actions/AppActions";
 import { axiosAuth } from "@axios/axiosAuth";
 import { isDev } from "@config";
 import { AppDispatchProps } from "app-context";
 
-export const deleteTaskFromList = async ({ appId, listId, taskId, id }: AppDispatchProps) => {
+export const deleteTaskFromList = async ({ appId, listId, taskId, id, dispatch }: AppDispatchProps) => {
   try {
     const { data } = await axiosAuth.delete(`app/${appId}/task-board/${id}/list/${listId}/task/${taskId}`);
-    console.log("data :>> ", data);
+    dispatch({ type: APP_ACTIONS.SET_REQUEST_STATUS, payload: "SUCCESS" });
+    dispatch({ type: APP_ACTIONS.SET_APP_TASKS, payload: data });
   } catch (error) {
     if (isDev) console.log("error", error);
   }
