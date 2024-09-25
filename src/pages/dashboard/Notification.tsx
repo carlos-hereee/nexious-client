e593e76fbbf77acac2c3f984bc92d294921005a8
carlos-hereee <97hernandez.c@gmail.com>
2024-09-25 16:26:35 -0500

    remove name of notification

--- src/pages/dashboard/Notification.tsx
+++ src/pages/dashboard/Notification.tsx
@@ -10,7 +10,7 @@ const Notification = ({ notifications, clearNotification }: Props) => {
   return (
     <div className="container">
       <h2 className="heading">Notifications</h2>
-      <Navigation menus={["Created", "Type", "Action taken", "Message", " "]} theme="navigation-bar hide-on-mobile" />
+      <Navigation menus={["Created", "Type", "Message", " "]} theme="navigation-bar hide-on-mobile" />
       {notifications.length > 0 ? (
         notifications.map((notification) => (
           <div key={notification.notificationId} className="notification-row highlight">
@@ -20,9 +20,9 @@ const Notification = ({ notifications, clearNotification }: Props) => {
             <p className="text-fit hide-on-mobile">
               <strong>{notification.category}</strong>
             </p>
-            <p className="text-fit">
+            {/* <p className="text-fit">
               <strong>{notification.name}</strong>
-            </p>
+            </p> */}
             {/* <ReadMore data={notification.message} uid="message" /> */}
             <p className="text-fit">{notification.message}</p>
             {/* {notification.link ? (
